dc416204e8ea8a5839f055a08920e87796ba7b61
Pavlos Vinieratos <pvinis@gmail.com>
2018-10-05 15:13:59 +0200

    version 0.35.0

--- RnDiffApp/index.android.js
+++ RnDiffApp/index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-class RnDiffApp extends Component {
+export default class RnDiffApp extends Component {
   render() {
     return (
       <View style={styles.container}>
