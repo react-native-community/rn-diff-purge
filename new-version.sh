fd794983bed99e01711d79f7b3f91505b514ec7f
Pavlos Vinieratos <pvinis@gmail.com>
2018-10-05 11:30:07 +0200

    new version script modification

--- new-version.sh
+++ new-version.sh
@@ -1,83 +1,57 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euxo pipefail
 
-newVersion=$2
+newVersion=$1
+projectName=RnDiffApp
 
-# Get the last branch
-lastGitBranch=$1
+# go to project branch
+git checkout project
 
-#  Checkout it
-git checkout $lastGitBranch
+# get previous version
+prevTag=$(git tag --points-at HEAD)
+prevVersion=$(basename "$prevTag")
 
-cd RnDiffApp
+# delete the old project
+rm -rf RnDiffApp
 
-# Save package.json for later use (for not being taken into account when computing stats)
-cp package.json ../package.json.temp
+# create a new project using the new version
+react-native init "$projectName" --version "$newVersion"
 
-# Install dependencies
-npm install
-
-# Launch RN upgrade
-react-native-git-upgrade $newVersion
-
-# Remove lock file, it is irrelevant for computing the diff
-rm package-lock.json
-
-# Strip version qualifier
-sed -i "" "s/\"react\": \"[\~\^]/\"react\": \"/" ./package.json
-sed -i "" "s/\"react-native\": \"[\~\^]/\"react-native\": \"/" ./package.json
-
-cd ..
-
-# Create the new branch
-git branch rn-$newVersion
-
-# Checkout it
-git checkout rn-$newVersion
-
-# Add modified files to commit
+# add new files and commit
 git add RnDiffApp
+git commit -m "version ${newVersion}"
 
-# Commit
-git commit -m "Output version ${newVersion}"
+# tag the commit with the new version
+git tag version/"${newVersion}"
 
-# Compute diff stats pour README.md
-## Restore the old package.json (we don't want to take it into account for stats)
-cp package.json.temp RnDiffApp/package.json
-## Save the stats
-diffStat=`git --no-pager diff HEAD~1 --shortstat`
-## Clean old package.json
-rm package.json.temp
-## Restore the new package.json
-git checkout -- RnDiffApp/package.json
+# save the stats
+diffStat=$(git --no-pager diff HEAD~1 --shortstat)
 
-# Print diff
+# print diff
 git --no-pager diff HEAD~1
 
-# Print all branches (inspired from the oh-my-zsh shortcut: "glola")
-git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all
+# ask for confirmation before pushing
+echo -e "\\nIs that OK ? (Yn)"
+read -r confirmation
 
-# Ask for confirmation before pushing
-echo -e "\nIs that OK ? (Yn)"
-read confirmation
-
-if [ -z $confirmation ] || [ $confirmation = "y" ] || [ $confirmation = "Y" ]
+if [ -z "$confirmation" ] || [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]
 then
-  echo -e "Push branch..."
+  echo -e "Pushing branch..."
 
-  # Push the branch
-  git push origin rn-$newVersion
+  # push the project branch
+  git push origin project
 fi
 
-# Come back to master branch
+# come back to master branch
 git checkout master
 
-# Update README.md
-diffUrl="[${lastGitBranch}...rn-${newVersion}](https://github.com/ncuillery/rn-diff/compare/${lastGitBranch}...rn-${newVersion})"
-patchUrl="[${lastGitBranch}...rn-${newVersion}](https://github.com/ncuillery/rn-diff/compare/${lastGitBranch}...rn-${newVersion}.diff)"
+# update readme
+diffUrl="[${prevVersion}...${newVersion}](https://github.com/pvinis/rn-diff-purge/compare/version${prevVersion}...version/${newVersion})"
+patchUrl="[${prevVersion}...${newVersion}](https://github.com/pvinis/rn-diff-purge/compare/version${prevVersion}...version/${newVersion}.diff)"
 
-# Insert a row in the version table
-## Insert a new line (a bit tricky but compatible with either OSX sed and GNU sed)
+# insert a row in the version table
+# shellcheck disable=SC1004
 sed -i '' 's/----|----|----|----/----|----|----|----\
 NEWLINE/g' README.md
-## Edit the new line with the version info
+# edit the new line with the version info
 sed -i "" "s@NEWLINE@${newVersion}|${diffUrl}|${patchUrl}|${diffStat}@" README.md
