522318efa1eb3fb6fbfc667484b0e986f16a2624
Pavlos Vinieratos <pvinis@gmail.com>
2022-06-01 17:37:00 +0100

    ok

--- new-release.sh
+++ new-release.sh
@@ -110,6 +110,7 @@ function pushMaster () {
     # commit and push
     git add .
     git commit -m "Add release $newRelease"
+    git push origin master "version/$newRelease"
 }
 
 function generateTable () {
