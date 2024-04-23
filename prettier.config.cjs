982aafe7db3162c0ed27c5c5eb578a40394fbc15
Pavlos Vinieratos <pvinis@gmail.com>
2024-04-23 16:32:08 +0300

    try

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
 	semi: false,
 	useTabs: true,
+	singleQuotes: false,
 }
---
+++ scripts/notify-new-release.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import core from "@actions/core"
+import github from "@actions/github"
+
+const repositoryData = {
+	owner: "react-native-community",
+	repo: "rn-diff-purge",
+}
+
+const releasesFileName = "RELEASES"
+
+;(async () => {
+	const client = process.argv[1]
+
+	await Promise.all(
+		github.context.payload.commits.map(async ({ id: commitRef }) => {
+			const {
+				data: { files },
+			} = await client.repos.getCommit({
+				...repositoryData,
+				ref: commitRef,
+			})
+
+			const [releaseFile] = files.filter(
+				({ filename }) => filename === releasesFileName
+			)
+
+			if (!releaseFile) {
+				core.debug(`No release file changed on commit ${commitRef}, moving on`)
+
+				return
+			}
+
+			core.debug(
+				`Release file changed on commit ${commitRef}, sending notification`
+			)
+
+			await client.repos.createDispatchEvent({
+				...repositoryData,
+				repo: "upgrade-support",
+				event_type: "NEW_RELEASE",
+			})
+		})
+	)
+})()
