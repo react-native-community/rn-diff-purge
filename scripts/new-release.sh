e430765dbd2a1cba21e1c52ed1f773ca8133968b
Pavlos Vinieratos <pvinis@gmail.com>
2024-06-07 21:21:39 +0300

    aha

--- scripts/new-release.sh
+++ scripts/new-release.sh
@@ -86,10 +86,10 @@ function addReleaseToList () {
 
     if command -v tac; then
         #   take each line ->dedup->    sort them              -> reverse them -> save them
-        cat "$ReleasesFile" | uniq | xargs yarn --silent semver | tac           > tmpfile
+        cat "$ReleasesFile" | uniq | xargs npx --silent semver | tac           > tmpfile
     else
         #   take each line ->dedup->    sort them              -> reverse them -> save them
-        cat "$ReleasesFile" | uniq | xargs yarn --silent semver | tail -r       > tmpfile
+        cat "$ReleasesFile" | uniq | xargs npx --silent semver | tail -r       > tmpfile
     fi
 
     mv tmpfile "$ReleasesFile"
