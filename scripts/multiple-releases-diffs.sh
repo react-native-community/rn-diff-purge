278c3ceea5089ea33484db6c1e420dddd1d454cc
Pavlos Vinieratos <pvinis@gmail.com>
2024-06-27 13:44:50 +0300

    regen some diffs

--- scripts/multiple-releases-diffs.sh
+++ scripts/multiple-releases-diffs.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-IFS=$'\n' GLOBIGNORE='*' command eval 'releases=($(tac RELEASES))'
+releases=(
+0.75.0-rc.2
+0.74.2
+0.74.1
+0.74.1-rc.0
+0.74.0
+0.74.0-rc.9
+0.74.0-rc.8
+0.74.0-rc.7
+0.74.0-rc.6
+0.74.0-rc.5
+0.74.0-rc.4
+0.74.0-rc.3
+0.74.0-rc.2
+0.74.0-rc.0
+)
+
+
+if [ ! -d wt-diffs ]; then
+  git worktree add wt-diffs diffs
+fi
 
 for vfrom in "${releases[@]}"
 do
   echo "from $vfrom"
   for vto in "${releases[@]}"
   do
-    git diff --binary -M15% origin/release/"$vfrom"..origin/release/"$vto" > wt-diffs/diffs/"$vfrom".."$vto".diff
+    git diff --binary -w -M15% origin/release/"$vfrom"..origin/release/"$vto" > wt-diffs/diffs/"$vfrom".."$vto".diff
   done
 done
