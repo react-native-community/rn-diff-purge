99e9bc4ab86b7740d704e6d5b1f04f5e2af3e1d1
Pavlos Vinieratos <pvinis@gmail.com>
2024-07-02 01:55:09 +0300

    aha

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,4 +2,14 @@ module.exports = {
 	semi: false,
 	useTabs: true,
 	singleQuotes: false,
+
+	overrides: [
+		{
+			files: "package.json",
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+			},
+		},
+	],
 }
