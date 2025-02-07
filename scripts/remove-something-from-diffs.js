53aa2d99b3dabffd31a73398221651acf1745bba
Pavlos Vinieratos <pvinis@gmail.com>
2025-02-07 17:16:48 +0200

    cleanup

--- scripts/remove-something-from-diffs.js
+++ scripts/remove-something-from-diffs.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import fs from "fs"
-import replace from "replace-in-file"
+import {replaceInFileSync} from "replace-in-file"
 
 const diffsDir = "wt-diffs/diffs"
 
@@ -16,14 +16,14 @@ const removeSomething = async () => {
 			// const fullpath = `wt-diffs/diffs/0.43.1..0.67.5.diff`
 			console.log(fullpath)
 
-			if (!fullpath.includes("..0.71.19.diff")) {
-				return
-			}
+			// if (!fullpath.includes("..0.71.19.diff")) {
+			// 	return
+			// }
 
 			// check on https://regex101.com
-			const toBeReplaced = /diff --git a\/RnDiffApp\/.yrc.yml .*?diff/s
+			const toBeReplaced = /diff --git a\/.gitignore b\/.gitignore.*?diff/s
 
-			replace.sync({
+			replaceInFileSync({
 				files: fullpath,
 				from: new RegExp(toBeReplaced),
 				to: "diff",
