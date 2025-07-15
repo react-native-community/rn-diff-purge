31f7f4a9fe7ea6eb8188fcaca5cea8c408651baa
Anka <runner@Mac-1752578787156.local>
2025-07-15 11:30:58 +0000

    Release 0.81.0-rc.1

---
+++ RnDiffApp/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </>
+  );
+}
+
+function AppContent() {
+  // TODO(huntie): Restore safeAreaInsets assignment (see #153)
+  return (
+    <View style={styles.container}>
+      <NewAppScreen templateFileName="App.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
