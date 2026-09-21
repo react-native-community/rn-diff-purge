afb9142249955ed0a93a9552226185e9a56de660
Anka <runner@sjc22-bt149-21a947d7-7a8c-4059-81a3-1957003734fa-AA78DF3304EC.local>
2026-09-21 18:06:45 +0000

    Release 0.88.0-rc.2

---
+++ RnDiffApp/ios/RnDiffApp/AppDelegate.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    true
+  }
+}
